3c4b9e4d55b75912491e092dea5a0808be07c4de
Bruno <brunomendes.2259@gmail.com>
2025-09-13 13:13:37 -0300

    correção do dockercompose,corrigindo caminho para banco de dados

--- Client/Database/create-db.sql
+++ Client/Database/create-db.sql
@@ -1,15 +1,3 @@
--- Criar o banco de dados se não existir
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'clientdb') THEN
-      CREATE DATABASE clientdb;
-   END IF;
-END
-$$;
-
-\c clientdb;
-
--- Criar a tabela de clientes se não existir
 CREATE TABLE IF NOT EXISTS clients (
     id UUID PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
